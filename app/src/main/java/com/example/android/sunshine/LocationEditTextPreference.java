607ce470e63f08310fb0201bb6916495fa34e540
Elorri <etchemendy.elorri@gmail.com>
2015-12-01 18:39:19 +0100

    Enable location ok button only if enough character

--- app/src/main/java/com/example/android/sunshine/LocationEditTextPreference.java
+++ app/src/main/java/com/example/android/sunshine/LocationEditTextPreference.java
@@ -1,14 +1,22 @@
 package com.example.android.sunshine;
 
+import android.app.Dialog;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
 import android.preference.EditTextPreference;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Elorri on 01/12/2015.
  */
 public class LocationEditTextPreference extends EditTextPreference {
+
     static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
     private int mMinLength;
 
@@ -23,5 +31,43 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
+    }
+
+
+    @Override
+    protected void showDialog(Bundle state) {
+        super.showDialog(state);
+
+        EditText et = getEditText();
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                Dialog d = getDialog();
+                //We know it's an AlertDialog because we have looked the EditTextPreference doc,
+                // but if it change in the future we don't want our app to crash. 
+                if (d instanceof AlertDialog) {
+                    AlertDialog dialog = (AlertDialog) d;
+                    Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                    // Check if the EditText is empty
+                    if (s.length() < mMinLength) {
+                        // Disable OK button
+                        positiveButton.setEnabled(false);
+                    } else {
+                        // Re-enable the button.
+                        positiveButton.setEnabled(true);
+                    }
+                }
+            }
+        });
     }
 }
